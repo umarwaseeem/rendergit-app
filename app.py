c2d750a3cc023a0ba4c3e7d620d260ff4868f77a
umarwaseeem <umar.waseem@gmail.com>
2025-08-20 14:02:14 +0500

    Refactor import statements for clarity and organization

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, Markup
+from flask import Flask, render_template, request
+from markupsafe import Markup
 from rendergit_core import render_repo_to_html
 
 app = Flask(__name__)
